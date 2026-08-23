e8b3d943181911ea0041aed6e971610c0f278fd2
Shaker-Al-Muzaini <sshakiralmazini@gmail.com>
2026-08-23 22:59:58 +0300

    CROD

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 from fastapi import FastAPI
 from pydantic import BaseModel ,HttpUrl
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World shaker"}
+    cursor.execute("SELECT * FROM studant")
+    data = cursor.fetchall()
+    return {"Hello": "World shaker", "studant": data}
 
 class tesigg(BaseModel):
     name: str
     age: int
-    website: HttpUrl 
+
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost', database='test', user='postgres', password='', 
+            cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print('Successfully connected Database')
+        break
+    except Exception as error:
+        print('Database connection failed')
+        print("Error:", error)
+        time.sleep(2)
+   
 
 @app.post("/items/")
 def view(item: tesigg):
-    return {"name": item.name, "age": item.age, "website": item.website}
+    return {"name": item.name, "age": item.age}
